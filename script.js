13898856301e288919dd107292ca6246515fa566
Cattleya Pia <cjpia612@gmail.com>
2020-05-01 18:43:17 -0700

    created and applied getter for local storage

--- script.js
+++ script.js
@@ -3,17 +3,20 @@ $(document).ready(function() {
     $("#currentDay").text(currentDay);
     
     var currentHour = moment().format("H");
-    console.log(currentHour);
+    // console.log(currentHour);
+
     timeBgColor();
     function timeBgColor() {
-    if ($(".hourRow").attr("value") === currentHour) {
-        $(".eventDescription").addClass("present");
-        } else if ($(".hourRow").attr("value") > currentHour) {
-            $(".eventDescription").addClass("past");
-        } else if ($(".hourRow").attr("value") < currentHour) {
-            $(".eventDescription").addClass("future");
-        }
-        // console.log($(".hourRow").attr("value"));
+    // if ($(".hourRow").attr("value") === currentHour) {
+    //     $(".eventDescription").addClass("present");
+
+    //     } else if ($(".hourRow").attr("value") > currentHour) {
+    //         $(".eventDescription").addClass("past");
+
+    //     } else if ($(".hourRow").attr("value") < currentHour) {
+    //         $(".eventDescription").addClass("future");
+    //     }
+    //     // console.log($(".hourRow").attr("value"));
     }   
     $(".saveBtn").on("click", function(){
         localStorage.setItem ("9", ($("#block9").val()))
@@ -27,4 +30,15 @@ $(document).ready(function() {
         localStorage.setItem ("17", ($("#block17").val()))
 
     });
+
+    
+    $("#block9").append(localStorage.getItem("9"));
+    $("#block10").append(localStorage.getItem("10"));
+    $("#block11").append(localStorage.getItem("11"));
+    $("#block12").append(localStorage.getItem("12"));
+    $("#block13").append(localStorage.getItem("13"));
+    $("#block14").append(localStorage.getItem("14"));
+    $("#block15").append(localStorage.getItem("15"));
+    $("#block16").append(localStorage.getItem("16"));
+    $("#block17").append(localStorage.getItem("17"));
 })
